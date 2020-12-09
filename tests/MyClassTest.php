6620c779c1f2898b59915a81d9bb4858e7877fb8
viktor <adamunt@mail.ru>
2020-12-09 21:33:32 +0300

    Code psr

--- tests/MyClassTest.php
+++ tests/MyClassTest.php
@@ -2,7 +2,8 @@
 
 use PHPUnit\Framework\TestCase;
 
-class MyClassTest extends TestCase {
+class MyClassTest extends TestCase
+{
     public function testReverseCharacters()
     {
         $this->assertEquals('iH, moM! erA uoy lla thgir?', MyClass::reverseCharacters('Hi, Mom! Are you all right?'));
